7440efb23fe50b28d178309d819d86da160078aa
billbsing <bill.barman@dex.sg>
2019-11-18 19:46:35 +0800

    docs

--- wallet_manager/key_chain.py
+++ wallet_manager/key_chain.py
@@ -4,31 +4,88 @@ import json
 
 class KeyChain():
     def __init__(self, filename):
+        """
+
+        Keychain to save local account encrypted private keys.
+
+        :param str filename: Filename of the keychain file.
+
+        """
         self._filename = filename
         self._key_list = {}
         self.load()
 
     def load(self):
+        """
+
+        Load the keychain into memory from the provided filename.
+
+        """
         if os.path.exists(self._filename):
             with open(self._filename, 'r') as fp:
                 self._key_list = json.load(fp)
 
     def save(self):
+        """
+
+        Save the keychain data to file, using the provided filename on class init.
+
+        """
         with open(self._filename, 'w') as fp:
             json.dump(self._key_list, fp)
 
     def get_key(self, address):
+        """
+        Return a JSON account details from the account address.
+
+        :param str address: Address of the account.
+
+        :return: JSON object of the account.
+
+        """
         return self._key_list.get(address, None)
 
     def set_key(self, address, key_item):
+        """
+
+        Set the account JSON object.
+
+        :param str address: Address of the account to set.
+        :param str key_item: JSON object to assign to this account.
+
+        """
         self._key_list[address] = key_item
 
     def delete_key(self, address):
+        """
+
+        Delete an account based on it's address.
+
+        :param str address: Address of the account to delete.
+
+        """
         del self._key_list[address]
 
     def is_key(self, address):
+        """
+
+        Return True if the address is being held in this Keychain.
+
+        :param str address: Address of the account to check.
+
+        :return: bool True if the address is in this Keychain.
+
+        """"
         return address in self._key_list
-        
+
     @property
     def address_list(self):
+        """
+
+        Return a list of addresses held in this keychain.
+
+        :return: List of addresses.
+        :type: list
+        
+        """
         return list(self._key_list.keys())
--- wallet_manager/wallet_manager.py
+++ wallet_manager/wallet_manager.py
@@ -18,13 +18,30 @@ def as_attrdict(val):
     return dict(val)
 
 class WalletManager():
-
     def __init__(self, key_chain_filename=None):
+        """
+        Create the wallet manager class.
+
+        :param str key_chain_filename: Optional key chain filename
+            that can be used for local access.
+
+        """
+
         if key_chain_filename:
             self._key_chain = KeyChain(key_chain_filename)
 
 
     def new_account(self, password, url=None):
+        """
+
+        Create a new account using the provided password, with a remote node or
+        if no URL provided as a local public/private key
+
+        :param str password: Password of the new account.
+        :param str url: URL of the remote parity node to connect and register the new
+
+        """
+
         address = None
         if url:
             web3 = Web3(HTTPProvider(url))
@@ -42,14 +59,39 @@ class WalletManager():
 
 
     def get_chain_status(self, url):
+        """
+
+        Get the current chain status.
+
+        :param str url: URL of the node on the block chain.
+        :return: The parity chain status.
+
+        """
         web3 = Web3(HTTPProvider(url))
         return web3.manager.request_blocking('parity_chainStatus', [])
 
     def get_chain_name(self, url):
+        """
+
+        Get the parity block chain name.
+
+        :param str url: URL of the parity block chain node.
+        :return: The parity chain name.
+
+        """
         web3 = Web3(HTTPProvider(url))
         return web3.manager.request_blocking('parity_chain', [])
 
     def delete_account(self, address, password, url=None):
+        """
+
+        Delete an account locally or on the parity node.
+
+        :param str address: Address of the account to delete.
+        :param str password: Password of the account to delete.
+        :param str url: URL of the parity node to delete on.
+
+        """
         if url:
             web3 = Web3(HTTPProvider(url))
             web3.manager.request_blocking('parity_killAccount', [address, password])
@@ -58,6 +100,17 @@ class WalletManager():
             self._key_chain.save()
 
     def list_accounts(self, url=None):
+        """
+
+        List the accounts on the node or locally saved in the key chain.
+
+        :param str url: URL of the parity node to list the accounts, if this
+            parameter is None then return the list of accounts in the key chain.
+
+        :return: List of accounts
+        :type: list
+
+        """
         result = None
         if url:
             web3 = Web3(HTTPProvider(url))
@@ -67,6 +120,18 @@ class WalletManager():
         return result
 
     def export_account_json(self, address, password, url=None):
+        """
+        Export the account details as a JSON record.
+
+        :param str address: Address of the account to export.
+        :param str password: Passmord of the account to export.
+        :param str url: URL of the node to export from, else if None then export
+        from the keychain.
+
+        :return: JSON object containing information about the account, including
+        the encrypted priwate key.
+
+        """
         if url:
             web3 = Web3(HTTPProvider(url))
             raw_data = web3.manager.request_blocking('parity_exportAccount', [address, password])
@@ -76,6 +141,17 @@ class WalletManager():
         return result
 
     def export_account_key(self, address, password, url=None):
+        """
+        Export the accounts private key. This is the best way to export
+        keys, as this exposes the private key and it can be easilly stolen.
+
+        :param str address: Address of the account to get the private key.
+        :param str password: Account password.
+        :param str url: URL of the parity node, or if None then use the key chain.
+
+        :return: Private key of the account.
+
+        """
         if url:
             web3 = Web3(HTTPProvider(url))
             raw_data = web3.manager.request_blocking('parity_exportAccount', [address, password])
@@ -85,6 +161,16 @@ class WalletManager():
         return EthAccount.decrypt(key_json, password)
 
     def import_account_json(self, json_text, password, url=None):
+        """
+
+        Import an account into the partiy node or local keychaain.
+
+        :param str json_text: JSON object of an account that has been exported.
+        :param str password: Password of the account that you wish to import.
+        :param str url: URL of the parity network you wish to import the account too.
+            If the URL is None, then import to the local keychain.
+
+        """
         if url:
             web3 = Web3(HTTPProvider(url))
             web3.manager.request_blocking('parity_newAccountFromWallet', [json_text, password])
@@ -95,6 +181,17 @@ class WalletManager():
             self._key_chain.save()
 
     def import_account_key(self, address, raw_key, password, url=None):
+        """
+
+        Import an account's private key. This is not a surggested solution, since passing an
+        account's private key is a security risk.
+
+        :param str address: Address of the account to import.
+        :param str raw_key: The raw private key of the account to import.
+        :param str password: The password to use for saving and importing the account.
+        :param str url: URL of the parity node of if None then import the account in the local
+            KeyChain.
+        """
         if url:
             web3 = Web3(HTTPProvider(url))
             address = web3.manager.request_blocking('parity_newAccountFromSecret', [raw_key, password])
@@ -104,10 +201,35 @@ class WalletManager():
         return address
 
     def balance_ether(self, address, url):
+        """
+
+        Retun the ether balance for an account.
+
+        :param str address: The address of the account.
+        :param str url: URL of the parity node to find the balance.
+
+        :return: The account balance.
+
+        """
         web3 = Web3(HTTPProvider(url))
         return web3.fromWei(web3.eth.getBalance(address), 'ether')
 
     def send_ether(self, from_address, password, to_address, amount, url=None, timeout=120, is_local=False):
+        """
+
+        Send some between two valid accounts. Remember the `to_address`, must be a known address,
+        if you send ether or Ocean tokens to a random or unkown address you cannot get
+        it back again.
+
+        :param str from_address: The address of the account to send ether from.
+        :param str password: Password of the from_address account that is sending the ether.
+        :param double amount: Amount to send in ether.
+        :param str url: URL of the node to use for sending ether.
+        :param int timeout: Timeout seconds to wait for transaction to complete ( defaults to 20 seconds).
+        :param bool is_local: Sign the transaction locally.
+
+        :return: Transaction receipt number.
+        """
         web3 = Web3(HTTPProvider(url))
 
         if is_local:
@@ -138,6 +260,14 @@ class WalletManager():
         return web3.eth.waitForTransactionReceipt(tx_hash, timeout=timeout)
 
     def get_ether(self, address, url):
+        """
+
+        Get ether from a farucet.
+
+        :param str address: Address for the account to get ether for.
+        :param str url: URL of the faucet to obtain ether.
+
+        """
         data  = {
             'address': address,
             'agent': 'server',
